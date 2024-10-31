4157a96e8f2415ecd04fb0d190e8066a40a0d766
Vixx-gs <victorgallegoserrate@gmail.com>
2024-10-31 09:19:17 +0100

    Funciones

--- Proyecto2/src/Ejercicios/main.java
+++ Proyecto2/src/Ejercicios/main.java
@@ -1,10 +1,26 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int n=0;
+		do {
+			try {
+				System.out.println("Selecciona un tipo de conversion: ");
+				System.out.println("1. Longitud");
+				System.out.println("2. Peso");
+				System.out.println("3. Temperatura");
+				String s = sc.nextLine();
+				n=Integer.parseInt(s);
+			}catch(Exception ex)
+			{
+				System.out.println("Introduce un nº correcto");
+			}
+		}while(n!=0);
+		
 	}
 
 }
