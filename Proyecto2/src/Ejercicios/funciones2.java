2413413df7067f9252eab122d4fbaed76387295a
Portatil <Portatil@DESKTOP-J4PO3AB>
2024-10-31 08:50:31 +0100

    Funciones de temperatura.

--- Proyecto2/src/Ejercicios/funciones2.java
+++ Proyecto2/src/Ejercicios/funciones2.java
@@ -1,10 +1,19 @@
 package Ejercicios;
 
 public class funciones2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//Esta funcion pasa de celsius a Farenheit
+	public static double celsiusFarenheit( double celsius) {
+		double farenheit=(celsius*1.8)+32;
+		return farenheit;
 	}
+	
+	//Esta funcion pasa de farenheit a Celsius
+	public static double farenheitCelsius(double farenheit) {
+		double celsius=(farenheit-32)*0.5556;
+		return celsius;
+	}
+	
 
 }
+ 
